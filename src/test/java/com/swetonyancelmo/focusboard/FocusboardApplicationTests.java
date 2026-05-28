372b1583b6a1568537497ba20e6f93baedffbe79
Swetony <swetonyancelmo@gmail.com>
2026-05-28 19:57:33 -0300

    chore: initial commit

---
+++ src/test/java/com/swetonyancelmo/focusboard/FocusboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swetonyancelmo.focusboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocusboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
