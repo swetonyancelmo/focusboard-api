bfaca9c423335af2f601a45bd20b4c6416b85b09
Swetony <swetonyancelmo@gmail.com>
2026-05-29 20:58:41 -0300

    feat: add a new metohd token repository

--- src/main/java/com/swetonyancelmo/focusboard/repository/RefreshTokenRepository.java
+++ src/main/java/com/swetonyancelmo/focusboard/repository/RefreshTokenRepository.java
@@ -3,11 +3,17 @@ package com.swetonyancelmo.focusboard.repository;
 import com.swetonyancelmo.focusboard.model.RefreshToken;
 import com.swetonyancelmo.focusboard.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
     Optional<RefreshToken> findByToken(String token);
-    void deleteByUser(User user);
+
+    @Modifying
+    @Query("DELETE FROM RefreshToken rt WHERE rt.user = :user")
+    void deleteByUser(@Param("user") User user);
 }
